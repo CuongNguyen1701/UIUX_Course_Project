2e6cc06fa495a9431552c2486fcf20bcc71ce959
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-05-12 18:40:39 +0700

    add source structure

--- regress/src/App.jsx
+++ regress/src/App.jsx
@@ -1,13 +1,20 @@
-
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <Routes>
+          <Navbar />
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<></>} />
+          <Route path="/user/auth/:id" element={<></>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- regress/tailwind.config.js
+++ regress/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "##194DD3",
+        secondary: "##4219D3",
+        tertiary: "##19AAD3",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
