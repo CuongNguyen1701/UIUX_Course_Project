f0b9594a0df5c710c2dcdaca77154c235270b7cd
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-05-13 23:18:29 +0700

    update logo

--- regress/src/components/LeftBar.jsx
+++ regress/src/components/LeftBar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/react.svg";
+import logo from "../assets/logo.png";
 import avatar from "../assets/avatar.png";
 const NavButton = ({ name, dest }) => {
   return (
     <li>
       <Link to={dest}>
-        <div className="p-5 font-bold rounded-xl text-black-100 hover:bg-primary">
+        <div className="py-2 pl-2 pr-10 font-bold rounded-lg text-black-100 hover:bg-primary-200">
           {name}
         </div>
       </Link>
@@ -15,15 +15,13 @@ const NavButton = ({ name, dest }) => {
 };
 const LeftBar = () => {
   return (
-    <aside className="fixed top-0 left-0 flex flex-col h-screen px-10 py-5 bg-white-100 w-fit left-bar">
-      <img
-        src={logo}
-        alt="Regress Logo"
-        className="flex self-center w-20 h-20"
-      />
+    <aside className="fixed top-0 left-0 flex flex-col h-screen px-10 py-5 bg-white w-fit left-bar">
+      <Link to="/" className="flex self-center w-20 h-20">
+        <img src={logo} alt="Regress Logo" />
+      </Link>
       <nav className="flex flex-col justify-between h-full pt-10">
         <div className="relative left-0">
-          <ul className="text-left">
+          <ul className="flex flex-col gap-1 text-left">
             <NavButton name="Trang chủ" dest="/" />
             <NavButton name="Dự án" dest="/projects" />
             <NavButton name="Thời gian biểu" dest="/schedule" />
