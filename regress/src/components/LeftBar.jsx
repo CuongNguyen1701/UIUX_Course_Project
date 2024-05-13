83fc6e5e28a44f5b177a42a2bfe380774fcfcceb
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-05-13 23:29:43 +0700

    design leftbar

--- regress/src/components/LeftBar.jsx
+++ regress/src/components/LeftBar.jsx
@@ -6,7 +6,7 @@ const NavButton = ({ name, dest }) => {
   return (
     <li>
       <Link to={dest}>
-        <div className="py-2 pl-2 pr-10 font-bold rounded-lg text-black-100 hover:bg-primary-200">
+        <div className="py-3 pl-2 pr-10 text-3xl rounded-xl text-black-100 hover:bg-primary-200">
           {name}
         </div>
       </Link>
@@ -16,29 +16,28 @@ const NavButton = ({ name, dest }) => {
 const LeftBar = () => {
   return (
     <aside className="fixed top-0 left-0 flex flex-col h-screen px-10 py-5 bg-white w-fit left-bar">
-      <Link to="/" className="flex self-center w-20 h-20">
+      <Link to="/" className="flex self-center w-24 h-24">
         <img src={logo} alt="Regress Logo" />
       </Link>
       <nav className="flex flex-col justify-between h-full pt-10">
         <div className="relative left-0">
           <ul className="flex flex-col gap-1 text-left">
             <NavButton name="Trang chủ" dest="/" />
-            <NavButton name="Dự án" dest="/projects" />
             <NavButton name="Thời gian biểu" dest="/schedule" />
+            <NavButton name="Dự án" dest="/projects" />
             <NavButton name="Dự đoán" dest="/predictions" />
           </ul>
         </div>
-        <ul>
-          <li>
-            <Link to="/personal"></Link>
-            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-violet-200">
-              <img src={avatar} className="size-10"></img>
-            </div>
-          </li>
-          <li>
+        <Link
+          to="/personal"
+          className="flex flex-row items-center self-center pr-5 text-black rounded-full w-fit h-fit bg-secondary-100"
+        >
+          <img src={avatar} className="size-20"></img>
+          Kuan Min LEE
+        </Link>
+        {/* <li>
             <Link to="/settings"></Link>
-          </li>
-        </ul>
+          </li> */}
       </nav>
     </aside>
   );
