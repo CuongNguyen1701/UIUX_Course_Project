ab573406422e2572c0c26e4c255dac74be82a1e5
Mingix-PT <mingixpt@gmail.com>
2024-05-16 07:08:08 +0700

    resolve conflict

--- regress/src/pages/SchedulePage.jsx
+++ regress/src/pages/SchedulePage.jsx
@@ -3,13 +3,8 @@ import { ScheduleBoard, Title } from "../components";
 
 const SchedulePage = () => {
     return (
-<<<<<<< HEAD
-        <div className="relative flex flex-col items-center justify-center h-screen w-screen bg-white">
-            <Tittle title="Thời gian biểu" />
-=======
         <div className="flex flex-col items-center justify-center min-h-screen">
             <Title title="Thời gian biểu" />
->>>>>>> refs/remotes/origin/main
             {/* <ScheduleBoard /> */}
         </div>
     );
