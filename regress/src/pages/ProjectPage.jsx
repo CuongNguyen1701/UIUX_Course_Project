c123619c3f74bee2d3a94703ad52fb5612b4972e
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-05-16 13:20:35 +0700

    add link to project

--- regress/src/pages/ProjectPage.jsx
+++ regress/src/pages/ProjectPage.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import { useState } from "react";
 import { LeftBar, Title, ScheduleBoard, LineChart } from "../components";
-const RecommendCard = ({ title, description, tag, process }) => {
+
+import { Link } from "react-router-dom";
+const RecommendCard = ({ key, title, description, tag, process }) => {
   let tagColor;
   let processColor;
   let processWidth;
@@ -32,7 +34,10 @@ const RecommendCard = ({ title, description, tag, process }) => {
   console.log(processColor);
   console.log(tagColor);
   return (
-    <div className="inline-flex flex-col items-center justify-start m-2 border rounded-md w-fit h-fit group border-black/opacity-10 hover:cursor-pointer">
+    <Link
+      to={`${key}`}
+      className="inline-flex flex-col items-center justify-start m-2 border rounded-md w-fit h-fit group border-black/opacity-10 hover:cursor-pointer"
+    >
       <div className="inline-flex items-start self-stretch justify-start h-40">
         <div className="relative w-56 h-40 bg-gray-100 group-hover:bg-primary-100">
           <div className="h-full w-full flex flex-row items-center justify-center text-center text-black text-2xl font-normal font-['Roboto'] leading-none">
@@ -61,11 +66,41 @@ const RecommendCard = ({ title, description, tag, process }) => {
           />
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
 const ProjectPage = () => {
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      title: "UIUX",
+      description: "Hanoi University of Science and Technology",
+      tag: "Ưu tiên",
+      process: 90,
+    },
+    {
+      id: 2,
+      title: "Giải tích 3",
+      description: "Hanoi University of Science and Technology",
+      tag: "Nên làm",
+      process: 40,
+    },
+    {
+      id: 3,
+      title: "NeuralCV",
+      description: "BusiTech BootCamp",
+      tag: "Nên làm",
+      process: 20,
+    },
+    {
+      id: 4,
+      title: "Intro AI",
+      description: "Hanoi University of Science and Technology",
+      tag: "Nên làm",
+      process: 60,
+    },
+  ]);
   return (
     <div className="relative flex flex-col justify-center w-screen min-h-screen bg-white">
       <Title title="Dự án" />
@@ -77,30 +112,15 @@ const ProjectPage = () => {
               <div className="text-2xl font-semibold text-gray-700">Gợi ý</div>
             </div>
             <div className="relative grid-flow-row grid-cols-2">
-              <RecommendCard
-                title="UIUX"
-                description="Hanoi University of Science and Technology"
-                tag="Ưu tiên"
-                process="90"
-              />
-              <RecommendCard
-                title="Giải tích 3"
-                description="Hanoi University of Science and Technology"
-                tag="Nên làm"
-                process="40"
-              />
-              <RecommendCard
-                title="NeuralCV"
-                description="BusiTech BootCamp"
-                tag="Nên làm"
-                process="20"
-              />
-              <RecommendCard
-                title="Intro AI"
-                description="Hanoi University of Science and Technology"
-                tag="Nên làm"
-                process="60"
-              />
+              {projects.map((project) => (
+                <RecommendCard
+                  title={project.title}
+                  description={project.description}
+                  tag={project.tag}
+                  process={project.process}
+                  key={project.id}
+                />
+              ))}
             </div>
           </div>
         </div>
