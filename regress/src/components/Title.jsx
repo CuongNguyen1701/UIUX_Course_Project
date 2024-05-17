115b11d383db478b3a9d79163403a854f3cc7ed8
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-05-17 14:19:09 +0700

    t

--- regress/src/components/Title.jsx
+++ regress/src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Title = ({ title }) => {
   return (
-    <h1 className="absolute w-8/12 h-16 mb-4 text-5xl text-gray-700 place-content-center left-1/4 top-10 bg-primary-300 rounded-xl">
+    <h1 className="absolute z-40 w-8/12 h-16 mb-4 text-5xl text-gray-700 place-content-center left-1/4 top-10 bg-primary-300 rounded-xl">
       {title}
     </h1>
   );
