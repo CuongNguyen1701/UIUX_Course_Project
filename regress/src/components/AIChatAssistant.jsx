f7fb506b0e53429d67aeff08ba88ceaee85a077b
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-05-17 13:05:14 +0700

    fix

--- regress/src/components/AIChatAssistant.jsx
+++ regress/src/components/AIChatAssistant.jsx
@@ -139,7 +139,7 @@ const AIChatAssistant = () => {
   };
   return (
     <Draggable bounds="body" defaultPosition={{ x: 100, y: 0 }}>
-      <div className="fixed z-10 flex flex-row m-10 bottom-10 right-40">
+      <div className="fixed z-50 flex flex-row m-10 bottom-10 right-40">
         {chatOpen && <ChatScreen chatLog={chatLog} insertChat={insertChat} />}
         <div className="select-none">
           <span
--- regress/src/pages/HomePage.jsx
+++ regress/src/pages/HomePage.jsx
@@ -16,7 +16,7 @@ const Notification = ({ title, content, time, unread }) => {
 };
 const NotificationBar = () => {
   return (
-    <div className="top-0 right-0 flex flex-col w-1/5 gap-5 px-10 py-8 bg-white border-2 border-gray-200 rounded-xl h-3/4">
+    <div className="top-0 right-0 flex flex-col w-1/6 gap-5 px-10 py-8 mr-10 bg-white border-2 border-gray-200 rounded-xl h-3/4">
       <div className="text-2xl font-bold text-left text-black">Thông báo</div>
       <div className="flex flex-row justify-start gap-10 text-center text-md">
         <div className="px-3 py-1 text-white rounded-full bg-primary">
